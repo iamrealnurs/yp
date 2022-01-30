54191cfb24888c761c427598947ec7042dd02a86
iamrealnurs <nurs567@yandex.ru>
2022-01-30 20:04:14 +0600

    Done second issue

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,7 +81,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Almaty'
 
 USE_I18N = True
 
@@ -92,6 +92,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['greetings'] = "Hello New World!!!"
+        return context
