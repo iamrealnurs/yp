6cdc55c36b70f1dfcfb5381d7e5e5ca9555ec30d
iamrealnurs <nurs567@yandex.ru>
2022-01-31 21:48:18 +0600

    done third issue

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Seller, Category, Tag, Ads
 
-# Register your models here.
+
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'ads_amount',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'ads_amount',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+class AdsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'category', 'seller', 'created_at', 'updated_at',)
+
+
+admin.site.register(Seller, SellerAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Ads, AdsAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Seller(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    @property
+    def ads_amount(self):
+        return self.ads.count()
+
+    class Meta:
+        verbose_name = "Продавец"
+        verbose_name_plural = "Продавцы"
+
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=255,
+        unique=True,
+        blank=False
+    )
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        blank=True,
+        null=True
+    )
+
+    @property
+    def ads_amount(self):
+        return self.ads.count()
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(
+        max_length=255,
+        unique=True,
+        blank=False
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
+
+
+class Ads(models.Model):
+    title = models.CharField(
+        max_length=255,
+        blank=False
+    )
+    description = models.TextField(null=True)
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        related_name='ads'
+    )
+    seller = models.ForeignKey(
+        'Seller',
+        on_delete=models.CASCADE,
+        related_name='ads'
+    )
+    tags = models.ManyToManyField('Tag', blank=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=True,
+        null=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        blank=True,
+        null=True
+    )
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+
