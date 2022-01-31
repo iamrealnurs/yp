d8ba6d88ff25130b418f36fdb60e6411addbc9f6
iamrealnurs <nurs567@yandex.ru>
2022-01-31 23:32:14 +0600

    all works

--- main/data_gen.py
+++ main/data_gen.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User
 from main.models import Seller, Ad, ArchiveAd, Tag, Category
+import random
+import string
+
+# print(random.choices(string.ascii_lowercase))
 
 list_of_tags = ['скидка', 'доставка', 'бесплатно', 'эко', 'первый', 'халяль', 'кошерно']
 list_of_categories = ['Авто', 'Еда', 'Игрушки', 'Собутыльники']
@@ -17,3 +21,15 @@ for i in list_of_categories:
 
 for i in Category.objects.all():
     print(i.__dict__)
+
+
+for i in range(1, 20):
+    desc = ''.join(random.choices(string.ascii_lowercase, k=35))
+
+    Ad.objects.create(
+        name=desc[:4],
+        description=desc,
+        category=Category.objects.first(),
+        seller=Seller.objects.first(),
+        price=random.randint(1000, 10000)/100,
+    )
