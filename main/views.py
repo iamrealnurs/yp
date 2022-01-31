2593b2ebb7df99ff17d753884b26ba85b51413d4
iamrealnurs <nurs567@yandex.ru>
2022-01-31 20:53:37 +0600

    fix some comments

--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 class IndexView(TemplateView):
     template_name = 'main/index.html'
+    extra_context = {'greetings' : "Hello New World!!!"}
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['greetings'] = "Hello New World!!!"
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['greetings'] = "Hello New World!!!"
+    #     return context
